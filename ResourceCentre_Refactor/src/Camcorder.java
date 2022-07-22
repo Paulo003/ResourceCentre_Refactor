72f898f0cfc7d72fbec78396444c07c4a1ef0bf1
Paulo003 <21009874@myrp.edu.sg>
2022-07-22 14:12:28 +0800

    Refactored comments

--- ResourceCentre_Refactor/src/Camcorder.java
+++ ResourceCentre_Refactor/src/Camcorder.java
@@ -1,4 +1,4 @@
-
+ 
 public class Camcorder extends Item{
 	private int opticalZoom;
 
@@ -12,8 +12,9 @@ public class Camcorder extends Item{
 	}
 	
 	public String toString(){
-		String output = "";
+		String output = super.toString();
 		// Write your codes here
+		output += String.format("%-63s %-20d", output, getOpticalZoom());
 		return output;
 	}
 }
--- ResourceCentre_Refactor/src/Item.java
+++ ResourceCentre_Refactor/src/Item.java
@@ -14,7 +14,12 @@ public class Item {
 	public String toString() {
 		
 		// Write your codes here
-		return null;
+		String itemInfo = String.format("%-10s %-30s %-10s %-10s",
+				assetTag,
+				description,
+				showAvailability(isAvailable),
+				dueDate);
+		return itemInfo;
 	}
 	public String getAssetTag() {
 		return assetTag;
@@ -39,5 +44,16 @@ public class Item {
 	public void setIsAvailable(boolean isAvailable) {
 		this.isAvailable = isAvailable;
 	}
-}
+	public static String showAvailability(boolean isAvailable) {
+		String avail;
+		
+		if (isAvailable == true) {
+			avail = "Yes";
+		} else {
+			avail = "No";
+		}
+		return avail;
+		}
+	}
+
 

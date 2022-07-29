ab57c1f27d12a9a37c8ae782c11a4c75b31f1731
Paulo003 <21009874@myrp.edu.sg>
2022-07-29 10:21:14 +0800

    comments on resourcecentre

--- ResourceCentre_Refactor/src/ResourceCentre.java
+++ ResourceCentre_Refactor/src/ResourceCentre.java
@@ -157,6 +157,7 @@ public class ResourceCentre {
 			output += String.format("%-84s \n", camcorderList.get(i).toString());
 		}
 		return output;
+		//wergethjy
 	}
 	public static void viewAllCamcorder(ArrayList<Camcorder> camcorderList) {
 		ResourceCentre.setHeader("CAMCORDER LIST");
